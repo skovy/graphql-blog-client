14124543f572faf1d606eafa07bba95884e639db
Spencer Miskoviak <smiskoviak@gmail.com>
2017-10-23 19:47:37 -0700

    add basics for TypeScript, Webpack, HTML Webpack Plugin, Babel and hello world React

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
