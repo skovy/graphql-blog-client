575fef106745c8b915f5d86e15b3a615f941cf01
Spencer Miskoviak <smiskoviak@gmail.com>
2017-11-05 20:17:13 -0800

    Add a component to display a single Post

---
+++ src/components/post/index.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import styled from "styled-components";
+
+import * as config from "config";
+import { Post as PostType } from "types";
+
+const PostTitle = styled.h3`
+  font-family: ${config.fonts.primary};
+  font-size: ${config.sizings.lg};
+  margin: 0 0 ${config.sizings.sm};
+`;
+
+const PostDesription = styled.p`
+  font-size: ${config.sizings.base};
+  margin: 0 0 ${config.sizings.lg};
+`;
+
+const PostMetadata = styled.p`
+  font-size: ${config.sizings.base};
+  margin: 0 0 ${config.sizings.sm};
+  text-transform: uppercase;
+  font-weight: ${config.weights.light};
+`;
+
+
+interface Props {
+  post: PostType;
+}
+
+class Post extends React.Component<Props> {
+  public render() {
+    const { post } = this.props;
+
+    return (
+      <div>
+        <PostMetadata>
+          Posted By {post.author && post.author.name}
+        </PostMetadata>
+        <PostTitle>
+          {post.title}
+        </PostTitle>
+        <PostDesription>
+          {post.text}
+        </PostDesription>
+      </div>
+    );
+  }
+}
+
+export default Post;
