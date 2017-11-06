b48bb094f126797a828a650521eddcfc3dc3b73b
Spencer Miskoviak <smiskoviak@gmail.com>
2017-11-05 17:21:47 -0800

    Add base container component

---
+++ src/components/container/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import styled from "styled-components";
+
+import * as config from "config";
+
+const Container = styled.div`
+  max-width: ${config.general.maxWidth};
+  margin: 0 auto;
+`;
+
+export default Container;
