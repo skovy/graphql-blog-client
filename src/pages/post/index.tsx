31caefe8e05d03ef7f16dd3b9cf8c6861157de6f
Spencer Miskoviak <smiskoviak@gmail.com>
2017-11-12 14:19:31 -0800

    Add basics for displaying a blog post

---
+++ src/pages/post/index.tsx
@@ -0,0 +1,68 @@
+import gql from "graphql-tag";
+import * as React from "react";
+import { ChildProps, graphql } from "react-apollo";
+
+import { Container } from "components/container";
+import * as config from "config";
+import { RouteComponentProps } from "react-router-dom";
+import { Post as PostType } from "types";
+
+// The result on the data prop from the GraphQL query
+interface Result {
+  post: PostType;
+}
+
+// The entire data prop from the GraphQL query and the props from the router
+type Props = ChildProps<RouteComponentProps<{ id: number }>, Result>;
+
+class PostBase extends React.Component<Props> {
+  public render() {
+    return (
+      <Container>
+        {this.renderContent()}
+      </Container>
+    );
+  }
+
+  private renderContent(): JSX.Element {
+    const { data } = this.props;
+
+    if (data && data.loading) {
+      return <div>Loading</div>;
+    } else if (data && data.error) {
+      return <div>{data.error}</div>;
+    } else if (data && !data.post) {
+      return <div>Post not found.</div>;
+    } else if (data && data.post) {
+      return (
+        <div>
+          <h2>{data.post.title}</h2>
+          <p>{data.post.text}</p>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+  }
+}
+
+const getPosts = gql`
+  query GetPost($id: ID!) {
+    post(id: $id) {
+      id
+      createdAt
+      text
+      title
+      author {
+        name
+      }
+    }
+  }
+`;
+
+const Post = graphql<Result, RouteComponentProps<{ id: number }>>(
+  getPosts,
+  { options: (props) => ({ variables: { id: props.match.params.id } }) }
+)(PostBase);
+
+export default Post;
