2e43727a283a3bb06fa52df45057eb7780479c8e
Spencer Miskoviak <smiskoviak@gmail.com>
2017-12-03 15:57:11 -0800

    Setup basics

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ApolloClient, createNetworkInterface } from "react-apollo";
+// import { ApolloClient, createNetworkInterface } from "react-apollo";
 import { ApolloProvider } from "react-apollo";
 import { render } from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -13,13 +13,22 @@ import { Write } from "containers/write";
 
 import "draft-js/dist/Draft.css";
 
+// const client = new ApolloClient({
+//   // configure the unique id for each object using the object type and id
+//   dataIdFromObject: (o: { __typename: string, id: string }) => `${o.__typename}:${o.id}`,
+//   networkInterface: createNetworkInterface({
+//     // configure the GraphQL endpoint
+//     uri: process.env.GRAPHQL_API_URI
+//   })
+// });
+
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+
 const client = new ApolloClient({
-  // configure the unique id for each object using the object type and id
-  dataIdFromObject: (o: { __typename: string, id: string }) => `${o.__typename}:${o.id}`,
-  networkInterface: createNetworkInterface({
-    // configure the GraphQL endpoint
-    uri: process.env.GRAPHQL_API_URI
-  })
+  link: new HttpLink({ uri: process.env.GRAPHQL_API_URI }),
+  cache: new InMemoryCache()
 });
 
 // Override the global body styles and add default styles
